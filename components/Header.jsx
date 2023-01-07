66bc691b11efd8790bd767dc4823f3c049cbd908
taleshrocha <taleshrochaz@gmail.com>
2023-01-06 22:13:44 -0300

    HeaderIcon

--- components/Header.jsx
+++ components/Header.jsx
@@ -13,6 +13,7 @@ import {
   SearchIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
+import HeaderIcon from "./HeaderIcon";
 
 function Header() {
   return (
@@ -25,12 +26,26 @@ function Header() {
           height={40}
           layout="fixed"
         />
-        <div className="flex ml-2 items-center rounded-full">
-          <SearchIcon className="h-6 text-blue-400" />
-          <input type="text" placeholder="Search Facebook" />
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <SearchIcon className="h-6 text-gray-600" />
+          <input
+            className="ml-2 bg-transparent outline-none placeholder-gray-500"
+            type="text"
+            placeholder="Search Facebook"
+          />
         </div>
       </div>
+
       {/* Center */}
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
 
       {/* Right */}
     </div>
