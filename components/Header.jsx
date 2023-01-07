02a5f3b02f46929325b13c90504af9d8ed2356e5
taleshrocha <taleshrochaz@gmail.com>
2023-01-06 22:49:23 -0300

    HeaderIcon center section done

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,7 +17,7 @@ import HeaderIcon from "./HeaderIcon";
 
 function Header() {
   return (
-    <div>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       {/* Left */}
       <div className="flex items-center">
         <Image
@@ -29,7 +29,7 @@ function Header() {
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-600" />
           <input
-            className="ml-2 bg-transparent outline-none placeholder-gray-500"
+            className="flex ml-2 bg-transparent outline-none placeholder-gray-500 flex-shrink"
             type="text"
             placeholder="Search Facebook"
           />
@@ -39,7 +39,7 @@ function Header() {
       {/* Center */}
       <div className="flex justify-center flex-grow">
         <div className="flex space-x-6 md:space-x-2">
-          <HeaderIcon Icon={HomeIcon} />
+          <HeaderIcon Icon={HomeIcon} active />
           <HeaderIcon Icon={FlagIcon} />
           <HeaderIcon Icon={PlayIcon} />
           <HeaderIcon Icon={ShoppingCartIcon} />
