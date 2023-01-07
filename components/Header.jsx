be25b923de0942998c1a26b7b6cff803f1f8dd00
taleshrocha <taleshrochaz@gmail.com>
2023-01-07 16:11:36 -0300

    Header done

--- components/Header.jsx
+++ components/Header.jsx
@@ -29,7 +29,7 @@ function Header() {
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-600" />
           <input
-            className="flex ml-2 bg-transparent outline-none placeholder-gray-500 flex-shrink"
+            className="hidden md:inline-flex ml-2 bg-transparent outline-none placeholder-gray-500 flex-shrink"
             type="text"
             placeholder="Search Facebook"
           />
@@ -48,6 +48,15 @@ function Header() {
       </div>
 
       {/* Right */}
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* Profile Pic */}
+
+        <p className="whitespace-nowrap font-semibold pr-3 ">Tales Rocha</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
     </div>
   );
 }
