662c740353b8724a1f66669788e5e3c31ab00eb0
taleshrocha <taleshrochaz@gmail.com>
2023-01-21 21:45:50 -0300

    Better implementation of AutoTextArea

--- components/AutoTextArea.jsx
+++ components/AutoTextArea.jsx
@@ -1,35 +1,14 @@
-import { useState, useEffect, useRef } from "react";
+export function AutoTextArea() {
 
-export function AutoTextArea(props) {
-  const textAreaRef = useRef(null);
-  const [text, setText] = useState("");
-  const [textAreaHeight, setTextAreaHeight] = useState("auto");
-  const [parentHeight, setParentHeight] = useState("auto");
-
-  useEffect(() => {
-    setParentHeight(`${textAreaRef.current.scrollHeight}px`);
-    setTextAreaHeight(`${textAreaRef.current.scrollHeight}px`);
-  }, [text]);
-
-  const onChangeHandler = (event) => {
-    setTextAreaHeight("auto");
-    setParentHeight(`${textAreaRef.current.scrollHeight}px`);
-    setText(event.target.value);
-
-    if (props.onChange) {
-      props.onChange(event);
-    }
-  };
+function handleKeyDown(e) {
+    e.target.style.height = 'inherit'
+    e.target.style.height = `${e.target.scrollHeight}px`
+  }
 
   return (
-    <div style={{ minHeight: parentHeight }}>
       <textarea
-        {...props}
-        onChange={onChangeHandler}
-        style={{ height: textAreaHeight }}
-        ref={textAreaRef}
+      onKeyDown={handleKeyDown}
         rows={1}
       ></textarea>
-    </div>
   );
 }
